a53f1226ac6f15f078d984ee47e0cc5d97ff5ee5
Cristian Iván Aráoz <araozcristian@gmail.com>
2022-07-11 12:05:47 -0300

    Ej 8

---
+++ EjerciciosTema8/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace EjerciciosTema8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            persona.setEdad("29");
+            persona.setNombre("Cristian Araoz");
+            persona.setTelefono("11224556");
+
+            Console.WriteLine(persona.getEdad());
+            Console.WriteLine(persona.getNombre());
+            Console.WriteLine(persona.getTelefono());
+        }
+    }
+
+    class Persona
+    {
+        private String edad;
+        private String nombre;
+        private String telefono;
+
+        public void setEdad(String edad)
+        {
+            this.edad = edad;
+        }
+
+        public void setNombre(String nombre)
+        {
+            this.nombre = nombre;
+        }
+        public void setTelefono(String telefono)
+        {
+            this.telefono = telefono;
+        }
+
+        public String getEdad()
+        {
+            return this.edad;
+        }
+
+        public String getNombre()
+        {
+            return this.nombre;
+        }
+        public String getTelefono()
+        {
+            return this.telefono;
+        }
+
+    }
+}
